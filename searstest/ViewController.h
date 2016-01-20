988032c1bea34376f359eb02a8378ed89ebbccbb
Wei TingTao <s952865@go.thu.edu.tw>
2016-01-20 12:09:10 -0600

    Initial Commit

---
+++ searstest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  searstest
+//
+//  Created by Wei TingTao on 1/20/16.
+//  Copyright © 2016 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ searstest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  searstest
+//
+//  Created by Wei TingTao on 1/20/16.
+//  Copyright © 2016 weitingtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
